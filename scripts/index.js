4b82197d5f569bc89e45f9503c94351464e37424
Elena Blazhenskikh <elena.blazhenskikh@gmail.com>
2022-01-31 21:15:36 +0300

    fit: render initial cards from box

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,53 @@
-let page = document.querySelector('.page');
-let editBTn = page.querySelector('.profile__edit-btn');
-let popUp = page.querySelector('.popup');
-let closeBtn = page.querySelector('.popup__close-btn');
-let editForm = page.querySelector('.edit-frm');
-let nameInput = editForm.querySelector('.edit-frm__item_type_name');
-let jobInput = editForm.querySelector('.edit-frm__item_type_description');
-let profileName = page.querySelector('.profile__name');
-let profileJob = page.querySelector('.profile__description');
+const initialElements = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const page = document.querySelector('.page');
+const editBTn = page.querySelector('.profile__edit-btn');
+const popUp = page.querySelector('.popup');
+const closeBtn = page.querySelector('.popup__close-btn');
+const editForm = page.querySelector('.edit-frm');
+const nameInput = editForm.querySelector('.edit-frm__item_type_name');
+const jobInput = editForm.querySelector('.edit-frm__item_type_description');
+const profileName = page.querySelector('.profile__name');
+const profileJob = page.querySelector('.profile__description');
+const elementTemplate = page.querySelector('.element-template').content;
+const elements = page.querySelector('.elements');
+
+function addElement(card) {
+  const newElement = elementTemplate.cloneNode(true);
+  newElement.querySelector('.element__photo').src = card.link;
+  newElement.querySelector('.element__name').innerText = card.name;
+
+  elements.appendChild (newElement);
+}
+
+function renderInitialElements() {
+  initialElements.forEach (addElement);
+}
 
 function openEditForm() {
   nameInput.value = profileName.textContent;
@@ -28,3 +69,5 @@ function formSubmitHandler (evt) {
 editForm.addEventListener('submit', formSubmitHandler);
 editBTn.addEventListener('click', openEditForm);
 closeBtn.addEventListener('click', closeEditForm);
+
+renderInitialElements();
